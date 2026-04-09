fb6ef063f130579481104d54553de596a275cf08
Saharsh Bhatnagar <saharsh.cse@gmail.com>
2026-04-09 23:45:29 +0530

    updated frontend fetching link

--- scripts/api.js
+++ scripts/api.js
@@ -1,4 +1,8 @@
-const API_BASE_URL = 'http://localhost:5000/api/tasks';
+const isLocalhost = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
+
+const API_BASE_URL = isLocalhost 
+    ? 'http://localhost:5000/api/tasks' 
+    : '';
 
 // GET
 async function fetchTasks() {
